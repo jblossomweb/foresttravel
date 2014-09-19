85e89fbb8cec14bf481ada0f9205969e0bde5de9
John Blossom <jblossom.web@gmail.com>
2014-09-19 10:50:44 -0400

    PHP 1: Organize the following list of items into an array variable.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+// PHP
+
+// 1. Organize the following list of items into an array variable.
+$var = array(
+	'Fruit'	=>	'Apple',
+	'Color'	=>	'Yellow',
+	'Number'=>	7, // in php, trailing commas are allowed.
+);
