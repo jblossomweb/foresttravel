aa9ef265d6ad88fef534767f9eefee513f1ebe0e
John Blossom <jblossom.web@gmail.com>
2014-09-19 11:29:45 -0400

    CSS 1: Style the unordered list as per the following instructions

--- index.php
+++ index.php
@@ -21,8 +21,30 @@ $json = json_decode($json);
 // 5. Place a condition inside the foreach loop so that the value in the 2nd element is displayed in lowercase.
 
 ?>
+<html>
+<head>
+<!-- CSS 1: Style the unordered list as per the following instructions -->
+<style>
+	ul{
+		list-style: none
+	}
+	ul li{
+		display: block;
+		background-color: #BADA55;
+		margin-bottom: 20px;
+		text-align: center;
+	}
+	ul li:hover{
+		background-color: #EFFEC7;
+	}
+</style>
+</head>
+<body>
 <ul>
 <?php foreach($json as $key=>$val): ?>
 	<li><?php echo $key; ?> = <?php echo ($key === 'Color' ? strtolower($val): $val); ?></li>
 <?php endforeach; ?>
 </ul>
+
+</body>
+</html>
